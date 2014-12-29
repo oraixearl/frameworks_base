dc0fcd01b41f5cd09e47219a95d801eb87c3e236
Stevespear426 <stephen.k.spear@gmail.com>
2014-12-29 16:03:42 -0200

    Add custom system animations [1/2]
    Change-Id: Ibeaa0f369279bfa94f2547c423483f7eff80bcb8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cr_strings.xml
    	core/res/res/values/cr_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4079,6 +4079,25 @@ public final class Settings {
          * @hide
          */
         public static final String QS_DATE_TIME_CENTER = "qs_date_time_center";
+        
+        /**  
+         * Custom System Animations
+         * @hide
+         */  
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+                "task_open_behind",
+        };  
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";        
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
