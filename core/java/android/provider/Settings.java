93729bd29013b51cd66038c1df0f14ebefcb10d8
Sam Mortimer <sam@mortimer.me.uk>
2016-05-19 01:52:17 -0400

    [1/2] frameworks/base: Battery and Notification Lights
            Pulled together from:
    
            Author: Roman Birg <roman@cyngn.com>
            Date:   Wed Jul 15 13:27:02 2015 -0700
            framework: add config_ledCanPulse overlay setting
            Some devices do not support pulsing LEDs. Allow them to hide pulse
            settings.
    
            Author: Roman Birg <roman@cyngn.com>
            Date:   Wed Dec 23 14:02:52 2015 -0800
            NotificationManager: always restore identity on notification error
            Change-Id: I3fb369c34098c366c3c943da0d7c7c64127387c8
            Signed-off-by: Roman Birg <roman@cyngn.com>
    
            Author: Steve Kondik <steve@cyngn.com>
            Date:   Sat Nov 7 17:58:42 2015 -0800
            noman: Fix AIOOB
            Change-Id: I6284b8f860a7792b75531fe648ff967b6f78ab6a
    
            Author: Altaf-Mahdi <altaf.mahdi@gmail.com>
            Date:   Sat Nov 7 17:48:01 2015 +0000
            notificationmanager: dont clear LED when its forced on by Settings
            LED is cleared when swiping the panel down to view notifications,
            keep the light on if it has been forced on by Settings app.
            Change-Id: I9c4f4c3928c8217737c5df18fa5251a539e1ba03
    
            Author: LuK1337 <priv.luk@gmail.com>
            Date:   Thu Oct 29 19:57:25 2015 +0100
            NotificationManager: Allow Settings app to publish a notifications without icon
            We need that for notification light settings.
            Change-Id: Id0984656fa4e071b7dfa3f88caa6c2ac01e6cce4
    
            Author: DvTonder <david.vantonder@gmail.com>
            Author: Ricardo Cerqueira <cyanogenmod@cerqueira.org>
            Date:   Mon Nov 4 00:57:51 2013 +0000
            Framework: Port CM9 features to CM10
            Change-Id: Ibd63116df90b06f6ce6adb8a0343059bbb999bfb
    
            Author: Pawit Pornkitprasan <p.pawit@gmail.com>
            Date:   Sun Dec 8 15:24:41 2013 +0700
            BatteryService: fix FC on boot until battery stat is present
            updateLightsLocked() can be called from CM's added SettingsObserver
            when battery stat is not present, causing an FC and a loop until
            battery stat is present.
            Change-Id: Ic4438fe50e98f1aa05ae1d0d26240bf9410fd92f
    
            Author: Sam Mortimer <sam@mortimer.me.uk>
            Date:   Tue Dec 31 16:22:05 2013 -0800
            [2/2] Framework: instant led test
            Adds support a new notification extra boolean
            EXTRA_FORCE_SHOW_LIGHTS.  Used by settings
            notification light picker to force lights on
            when the screen is on.
            Change-Id: If0a42d32b28fe8c02ef5f7dd148db7eb478fac17
    
            Author: Danny Baumann <dannybaumann@web.de>
            Date:   Wed Apr 24 11:09:55 2013 +0200
            Allow creating custom dialogs in DialogPreference.
            This allows creating non-AlertDialogs while still using the click and
            state handling of DialogPreference.
            Change-Id: I54fd7d6d6b9f6a49fef1ae95e0178838b3edfa56
    
            Author: Michael Bestas <mikeioannina@gmail.com>
            Date:   Mon Aug 18 04:56:28 2014 +0300
            Add support for single color notification LED (1/2)
            Change-Id: I367af77036da9e87c6dd0df552ce4c56d945a44d
    
    PureNexus Edits:
    *remove call/voicemail led coloring
    
    Change-Id: I36d357779e1ff2f7b8f851392d4f2021a405ab5d
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/BatteryService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3598,6 +3598,38 @@ public final class Settings {
          * @hide
          */
         public static final String REVERSE_LOOKUP_PROVIDER = "reverse_lookup_provider";
+        
+        /**
+         * What color to use for the battery LED while charging - low
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_LOW_COLOR = "battery_light_low_color";
+
+        /**
+         * What color to use for the battery LED while charging - medium
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_MEDIUM_COLOR = "battery_light_medium_color";
+
+        /**
+         * What color to use for the battery LED while charging - full
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_FULL_COLOR = "battery_light_full_color";
+
+        /**
+         * Whether the battery light should be enabled (if hardware supports it)
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_ENABLED = "battery_light_enabled";
+
+        /**
+         * Whether the battery LED should repeatedly flash when the battery is low
+         * on charge. The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String BATTERY_LIGHT_PULSE = "battery_light_pulse";        
 
         /**
          * Whether user can swap the order of the Alert Slider.
@@ -3696,6 +3728,41 @@ public final class Settings {
          * @hide
          */
         public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+        
+        /**
+         * What color to use for the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_COLOR =
+                "notification_light_pulse_default_color";
+
+        /**
+         * How long to flash the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_ON =
+                "notification_light_pulse_default_led_on";
+
+        /**
+         * How long to wait between flashes for the notification LED by default
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_DEFAULT_LED_OFF =
+                "notification_light_pulse_default_led_off";
+
+        /**
+         * Whether to use the custom LED values for the notification pulse LED.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_ENABLE =
+                "notification_light_pulse_custom_enable";
+
+        /**
+         * Which custom LED values to use for the notification pulse LED.
+         * @hide
+         */
+        public static final String NOTIFICATION_LIGHT_PULSE_CUSTOM_VALUES =
+                "notification_light_pulse_custom_values";        
 
         /**
          * The delay when killing the app on long press takes place
