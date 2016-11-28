ff8db101ae4d8b37c775fde133bd81a79c28e501
roguesyko <roguesyko@gmail.com>
2016-11-28 18:46:45 -0500

    Fix path
    Change-Id: I1719fc79901c015abdd17ae29a22e41471bfc3ce

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -60,7 +60,7 @@ endif
 LOCAL_JAVA_LIBRARIES += org.slim.framework
 LOCAL_STATIC_JAVA_LIBRARIES += android-opt-cards
 LOCAL_AAPT_FLAGS += --extra-packages com.android.cards
-LOCAL_FULL_LIBS_MANIFEST_FILES := frameworks/opt/slim/packages/SlimSystemUI/AndroidManifest.xml
+LOCAL_FULL_LIBS_MANIFEST_FILES := frameworks/opt/reaper/packages/SlimSystemUI/AndroidManifest.xml
 LOCAL_SRC_FILES += $(call all-java-files-under, ../../../opt/slim/packages/SlimSystemUI/src)
 LOCAL_RESOURCE_DIR += frameworks/opt/cards/res frameworks/opt/slim/packages/SlimSystemUI/res
 
