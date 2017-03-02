benschhold/android_frameworks_base

Add BT easy toggle [1/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -19,6 +19,7 @@ package com.android.systemui.qs.tiles;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -86,16 +87,23 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
 
     @Override
     protected void handleClick() {
-        if (!mController.canConfigBluetooth()) {
-            mHost.startActivityDismissingKeyguard(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
-            return;
+        boolean easyToggle = isBtEasyToggleEnabled();
+            if (easyToggle) {
+                final boolean isEnabled = (Boolean)mState.value;
+                MetricsLogger.action(mContext, getMetricsCategory(), !isEnabled);
+	        mController.setBluetoothEnabled(!isEnabled);
+            } else {
+                if (!mController.canConfigBluetooth()) {
+	            mHost.startActivityDismissingKeyguard(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
+    	            return;
+    	        }
+                if (!mState.value) {
+                    mState.value = true;
+	            mController.setBluetoothEnabled(true);
+                }
+            showDetail(true);
+            }
         }
-        if (!mState.value) {
-            mState.value = true;
-            mController.setBluetoothEnabled(true);
-        }
-        showDetail(true);
-    }
 
     @Override
     public CharSequence getTileLabel() {
@@ -155,6 +163,11 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
                 R.string.accessibility_quick_settings_open_settings, getTileLabel());
         state.expandedAccessibilityClassName = Button.class.getName();
         state.minimalAccessibilityClassName = Switch.class.getName();
+    }
+
+    public boolean isBtEasyToggleEnabled() {
+        return Settings.System.getInt(mContext.getContentResolver(),
+            Settings.System.QS_BT_EASY_TOGGLE, 0) == 1;
     }
 
     @Override

oraixearl/frameworks_base

packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -20,6 +20,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.provider.Settings;
 import android.text.TextUtils;
@@ -87,14 +88,21 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
 
     @Override
     protected void handleClick() {
-        if (!mController.canConfigBluetooth()) {
-            mHost.startActivityDismissingKeyguard(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
-            return;
-        }
-        showDetail(true);
-        if (!mState.value) {
-            mState.value = true;
-            mController.setBluetoothEnabled(true);
+        boolean easyToggle = isBtEasyToggleEnabled();
+        if (easyToggle) {
+            final boolean isEnabled = (Boolean)mState.value;
+            MetricsLogger.action(mContext, getMetricsCategory(), !isEnabled);
+	     mController.setBluetoothEnabled(!isEnabled);
+         } else {
+            if (!mController.canConfigBluetooth()) {
+	        mHost.startActivityDismissingKeyguard(new Intent(Settings.ACTION_BLUETOOTH_SETTINGS));
+    	        return;
+    	     }
+            showDetail(true);
+            if (!mState.value) {
+               mState.value = true;
+	        mController.setBluetoothEnabled(true);
+            }
         }
     }
 
@@ -156,6 +164,11 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
                 R.string.accessibility_quick_settings_open_settings, getTileLabel());
         state.expandedAccessibilityClassName = Button.class.getName();
         state.minimalAccessibilityClassName = Switch.class.getName();
+    }
+
+    public boolean isBtEasyToggleEnabled() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+            Settings.Secure.QS_BT_EASY_TOGGLE, 0) == 1;
     }
 
     @Override